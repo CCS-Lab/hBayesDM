cfba0938cbcbcddbbb3adcf94d12b050504f2f54
Young Ahn <wooyoung.ahn@gmail.com>
2016-12-27 22:24:03 -0500

    Fixed an error

--- R/rhat.R
+++ R/rhat.R
@@ -10,18 +10,19 @@
 #' @return 
 #' If \code{'less'} is specified, then \code{rhat(fit, less)} will return \code{TRUE} if all Rhat values are
 #' less than or equal to \code{'less'}. If any values are greater than \code{'less'}, \code{rhat(fit, less)} will 
-#' return \code{FALSE}. If \code{'less'} is left unspecified, \code{rhat(fit)} will return a \code{data.frame} object 
+#' return \code{FALSE}. If \code{'less'} is left unspecified (NULL), \code{rhat(fit)} will return a \code{data.frame} object 
 #' containing all Rhat values. 
 #'
 #' @export
 
-rhat <- function(fit = NULL, less = F) {
+rhat <- function(fit = NULL, less = NULL) {
   if (class(fit)!="hBayesDM") {
     stop("Error: The 'fit' object is not of class hBayesDM!")
   } else {
-    rhatData <- data.frame(Rhat = summary(fit$fit)$summary[,"Rhat"])
+    summaryData <- rstan::summary(fit$fit)
+    rhatData <- data.frame(Rhat = summaryData[["summary"]][, "Rhat"])
   }
-  if (less!=F) { 
+  if (!is.null(less)) { 
     if (all(rhatData$Rhat<=less)) {
       cat("TRUE: All Rhat values are less than ", less, sep="")
     } else {
