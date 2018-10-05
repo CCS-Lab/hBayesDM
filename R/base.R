4439bb7944d5eeb1657f930a6a56893bbcaa4299
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2018-10-05 16:51:44 +0900

    Modify base.R & Add flags.R

--- R/base.R
+++ R/base.R
@@ -1,4 +1,22 @@
-model_base <- function() {
+#' @param model_name The name of the model.
+#' @param model_code Filename for the corresponding stan file.
+#' e.g., ra_prospect.stan
+#' @param datapath_example Path
+#' @param
+#' @param
+#' @param
+#' @param func_preprocess A function to preprocess the raw data.
+#' It should take one argument, \\code{rawdata}.
+#' It should return a list object with 4 keys: ID, N, T, Tsubj.
+
+model_base <- function(model_name,
+                       model_code,
+                       datapath_example,
+                       model_params,
+                       available_model_regressor,
+                       model_regressors,
+                       func_preprocess,
+                       ) {
   ret <- function(data           = "choose",
                   niter          = 4000,
                   nwarmup        = 1000,
@@ -15,114 +33,73 @@ model_base <- function() {
                   stepsize       = 1,
                   max_treedepth  = 10) {
 
-    # Path to .stan model file
-    if (modelRegressor) { # model regressors (for model-based neuroimaging, etc.)
-      stop("** Model-based regressors are not available for this model **\n")
+    # model regressors (for model-based neuroimaging, etc.)
+    if (modelRegressor) {
+      if (available_model_regressor) {
+        cat("************************************\n")
+        cat("** Extract model-based regressors **\n")
+        cat("************************************\n")
+      } else {
+        stop("** Model-based regressors are not available for this model **\n")
+      }
     }
-    # if (modelRegressor) { # model regressors (for model-based neuroimaging, etc.)
-    #   cat("************************************\n")
-    #   cat("** Extract model-based regressors **\n")
-    #   cat("************************************\n")
-    # }
 
     # To see how long computations take
     startTime <- Sys.time()
 
     # For using example data
     if (data == "example") {
-      data <- system.file("extdata", "ra_exampleData.txt", package = "hBayesDM")
+      data <- system.file("extdata", datapath_example, package = "hBayesDM")
     } else if (data == "choose") {
       data <- file.choose()
     }
 
     # Load data
     if (file.exists(data)) {
-      rawdata <- read.table( data, header = T, sep = "\t")
+      rawdata <- read.table(data, header = TRUE, sep = "\t")
     } else {
       stop("** The data file does not exist. Please check it again. **\n  e.g., data = '/MyFolder/SubFolder/dataFile.txt', ... **\n")
     }
+
     # Remove rows containing NAs
-    NA_rows_all = which(is.na(rawdata), arr.ind = T)  # rows with NAs
+    NA_rows_all = which(is.na(rawdata), arr.ind = TRUE)  # rows with NAs
     NA_rows = unique(NA_rows_all[, "row"])
     if (length(NA_rows) > 0) {
       rawdata = rawdata[-NA_rows,]
       cat("The number of rows with NAs = ", length(NA_rows), ". They are removed prior to modeling the data. \n", sep = "")
     }
 
-    # Individual Subjects
-    subjList <- unique(rawdata[,"subjID"]) # list of subjects x blocks
-    numSubjs <- length(subjList)           # number of subjects
-
     # Specify the number of parameters and parameters of interest
-    numPars <- 3
-    POI     <- c("mu_rho", "mu_lambda", "mu_tau",
-                 "sigma",
-                 "rho" , "lambda", "tau",
-                 "log_lik")
+    # model_params <- c("rho", "lambda", "tau")
+    numPars <- lenght(model_params)
+
+    POI <- c("log_lik")
+    POI <- c(POI, mapply(function(x) paste0("mu_", x), model_params))
+    POI <- c(POI, "sigma")
+    POI <- c(POI, model_params)
+
+    if (modelRegressor) {
+      POI <- c(POI, model_regressors)
+    }
 
     if (inc_postpred) {
       POI <- c(POI, "y_pred")
     }
 
-    modelName <- "ra_prospect"
-
-    # Information for user
-    cat("\nModel name = ", modelName, "\n")
-    cat("Data file  = ", data, "\n")
-    cat("\nDetails:\n")
-    if (vb) {
-      cat(" # Using variational inference # \n")
-    } else {
-      cat(" # of chains                   = ", nchain, "\n")
-      cat(" # of cores used               = ", ncore, "\n")
-      cat(" # of MCMC samples (per chain) = ", niter, "\n")
-      cat(" # of burn-in samples          = ", nwarmup, "\n")
-    }
-    cat(" # of subjects                 = ", numSubjs, "\n")
+    modelName <- model_name
 
     ################################################################################
     # THE DATA.  ###################################################################
     ################################################################################
 
-    Tsubj <- as.vector( rep( 0, numSubjs)) # number of trials for each subject
-
-    for (sIdx in 1:numSubjs)  {
-      curSubj     <- subjList[ sIdx]
-      Tsubj[sIdx] <- sum( rawdata$subjID == curSubj)  # Tsubj[N]
-    }
-
-    maxTrials <- max(Tsubj)
-
-    # Information for user continued
-    cat(" # of (max) trials per subject = ", maxTrials, "\n\n")
-
-    # for multiple subjects
-    gain    <- array(0, c(numSubjs, maxTrials))
-    loss    <- array(0, c(numSubjs, maxTrials))
-    cert    <- array(0, c(numSubjs, maxTrials))
-    gamble  <- array(-1, c(numSubjs, maxTrials))
-
-    for (i in 1:numSubjs) {
-      curSubj      <- subjList[i]
-      useTrials    <- Tsubj[i]
-      tmp          <- subset(rawdata, rawdata$subjID == curSubj)
-      gain[i, 1:useTrials]    <- tmp[1:useTrials, "gain"]
-      loss[i, 1:useTrials]    <- abs(tmp[1:useTrials, "loss"])  # absolute loss amount
-      cert[i, 1:useTrials]    <- tmp[1:useTrials, "cert"]
-      gamble[i, 1:useTrials]  <- tmp[1:useTrials, "gamble"]
-    }
-
-    dataList <- list(
-      N       = numSubjs,
-      T       = maxTrials,
-      Tsubj   = Tsubj,
-      numPars = numPars,
-      gain    = gain,
-      loss    = loss,
-      cert    = cert,
-      gamble  = gamble)
+    dataList <- func_preprocess(rawdata)
+    numSubjs <- dataList[['N']]
+    subjList <- dataList[['ID']]
+    Tsubj <- dataList[['Tsubj']]
+    maxTrials <- dataList[['T']]
 
     # inits
+    # TODO: Make another common function to define the initial values.
     if (inits[1] != "random") {
       if (inits[1] == "fixed") {
         inits_fixed <- c(1.0, 1.0, 1.0)
@@ -140,7 +117,7 @@ model_base <- function() {
           rho_p    = rep(qnorm( inits_fixed[1]/2), numSubjs),
           lambda_p = rep(qnorm( inits_fixed[2]/5), numSubjs),
           tau_p    = rep(qnorm( inits_fixed[3]/5), numSubjs)
-  )
+        )
       }
     } else {
       genInitList <- "random"
@@ -158,12 +135,37 @@ model_base <- function() {
       options(mc.cores = 1)
     }
 
+    # Information for user
+    cat("\n")
+    cat("Model name = ", modelName, "\n")
+    cat("Data file  = ", data, "\n")
+    cat("\n")
+    cat("Details:\n")
+    if (vb) {
+      cat(" # Using variational inference # \n")
+    } else {
+      cat(" # of chains                   = ", nchain, "\n")
+      cat(" # of cores used               = ", ncore, "\n")
+      cat(" # of MCMC samples (per chain) = ", niter, "\n")
+      cat(" # of burn-in samples          = ", nwarmup, "\n")
+    }
+    cat(" # of subjects                 = ", numSubjs, "\n")
+
+    # Information for user continued
+    cat(" # of (max) trials per subject = ", maxTrials, "\n\n")
+
     cat("***********************************\n")
     cat("**  Loading a precompiled model  **\n")
     cat("***********************************\n")
 
     # Fit the Stan model
-    m = stanmodels$ra_prospect
+    if (FLAG_CRAN_VERSION) {
+      modelPath <- system.file("stan", model_code, package="hBayesDM")
+      m = rstan::stan_model(modelPath)
+    } else {
+      m = stanmodels[[gsub('.stan', '', model_code)]]
+    }
+
     if (vb) {   # if variational Bayesian
       fit = rstan::vb(m,
                       data   = dataList,
@@ -182,15 +184,12 @@ model_base <- function() {
                                            max_treedepth = max_treedepth,
                                            stepsize      = stepsize))
     }
+
     # Extract the Stan fit object
-    parVals <- rstan::extract(fit, permuted = T)
+    parVals <- rstan::extract(fit, permuted = TRUE)
     if (inc_postpred) {
       parVals$y_pred[parVals$y_pred == -1] <- NA
     }
-
-    rho    <- parVals$rho
-    lambda <- parVals$lambda
-    tau    <- parVals$tau
 
     # Individual parameters (e.g., individual posterior means)
     measureIndPars <- switch(indPars, mean=mean, median=median, mode=estimate_mode)
@@ -198,20 +197,39 @@ model_base <- function() {
     allIndPars <- as.data.frame(allIndPars)
 
     for (i in 1:numSubjs) {
-      allIndPars[i,] <- c(measureIndPars(rho[, i]),
-                          measureIndPars(lambda[, i]),
-                          measureIndPars(tau[, i]))
+      allIndPars[i,] <- mapply(function(param) {
+        measureIndPars(parVals[[param]][, i])
+      }, model_params)
     }
 
-    allIndPars           <- cbind(allIndPars, subjList)
-    colnames(allIndPars) <- c("rho",
-                              "lambda",
-                              "tau",
-                              "subjID")
+    allIndPars           <- cbind(subjList, allIndPars)
+    colnames(allIndPars) <- c("subjID", model_params)
 
     # Wrap up data into a list
-    modelData        <- list(modelName, allIndPars, parVals, fit, rawdata)
-    names(modelData) <- c("model", "allIndPars", "parVals", "fit", "rawdata")
+    modelData                 <- list()
+    modelData$model           <- modelName
+    modelData$allIndPars      <- allIndPars
+    modelData$parVals         <- parVals
+    modelData$fit             <- fit
+    modelData$rawdata         <- rawdata
+    modelData$modelRegressor  <- NA
+
+    # # Example code for Model Regressors
+    # # Imported from PRL_RL
+    # if (modelRegressor) {
+    #   ev_c  <- apply(parVals$mr_ev_c, c(2, 3), measureIndPars)
+    #   ev_nc <- apply(parVals$mr_ev_nc, c(2, 3), measureIndPars)
+    #   pe    <- apply(parVals$mr_pe, c(2, 3), measureIndPars)
+
+    #   # Initialize modelRegressor and add model-based regressors
+    #   regressors        <- NULL
+    #   regressors$ev_c   <- ev_c
+    #   regressors$ev_nc  <- ev_nc
+    #   regressors$pe     <- pe
+
+    #   modelData$modelRegressor <- regressors
+    # }
+
     class(modelData) <- "hBayesDM"
 
     # Total time of computations
@@ -231,7 +249,8 @@ model_base <- function() {
     }
 
     # Inform user of completion
-    cat("\n************************************\n")
+    cat("\n")
+    cat("************************************\n")
     cat("**** Model fitting is complete! ****\n")
     cat("************************************\n")
 
@@ -240,3 +259,47 @@ model_base <- function() {
 
   return (ret)
 }
+
+func_preprocess <- function(rawdata) {
+  # Individual Subjects
+  subjList <- unique(rawdata[,"subjID"]) # list of subjects x blocks
+  numSubjs <- length(subjList)           # number of subjects
+
+  Tsubj <- as.vector(rep(0, numSubjs)) # number of trials for each subject
+
+  for (sIdx in 1:numSubjs)  {
+    curSubj     <- subjList[ sIdx]
+    Tsubj[sIdx] <- sum( rawdata$subjID == curSubj)  # Tsubj[N]
+  }
+
+  maxTrials <- max(Tsubj)
+
+  # for multiple subjects
+  gain    <- array(0, c(numSubjs, maxTrials))
+  loss    <- array(0, c(numSubjs, maxTrials))
+  cert    <- array(0, c(numSubjs, maxTrials))
+  gamble  <- array(-1, c(numSubjs, maxTrials))
+
+  for (i in 1:numSubjs) {
+    curSubj      <- subjList[i]
+    useTrials    <- Tsubj[i]
+    tmp          <- subset(rawdata, rawdata$subjID == curSubj)
+    gain[i, 1:useTrials]    <- tmp[1:useTrials, "gain"]
+    loss[i, 1:useTrials]    <- abs(tmp[1:useTrials, "loss"])  # absolute loss amount
+    cert[i, 1:useTrials]    <- tmp[1:useTrials, "cert"]
+    gamble[i, 1:useTrials]  <- tmp[1:useTrials, "gamble"]
+  }
+
+  dataList <- list(
+    ID      = subjList,
+    N       = numSubjs,
+    T       = maxTrials,
+    Tsubj   = Tsubj,
+    gain    = gain,
+    loss    = loss,
+    cert    = cert,
+    gamble  = gamble)
+
+  return(dataList)
+}
+
