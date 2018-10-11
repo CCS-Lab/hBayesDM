122bb65b5f38bb97c463398612fe2f15ea276842
Jethro Lee <dlemfh96@gmail.com>
2018-10-12 05:12:44 +0900

    Complete the rest of model_base.R

--- R/model_base.R
+++ R/model_base.R
@@ -2,12 +2,9 @@
 #' @param model_name Name of the model, e.g. \code{"m1"}.
 #' @param model_type One of the following three: \code{NULL} OR \code{"single"} OR \code{"multipleB"}.
 #' @param data_columns Names of the necessary columns for the data, e.g. \code{c("subjID", "cue", "keyPressed", "outcome")}. Must be the entirety of necessary data columns that will be used at some point in the code, i.e. must always include \code{"subjID"}, and should include \code{"block"} in the case of 'multipleB' type models.
-#' @param parameters Names of the parameters of this model, e.g. \code{c("xi", "ep", "rho")}.
-#' @param regressors Names of the model-based regressors, e.g. \code{c("Qgo", "Qnogo", "Wgo", "Wnogo")}, OR if model-based regressors are not available for this model, \code{NULL}.
-#' @param preprocess_function The model-specific function to preprocess the raw data to pass to Stan.
-
-# TODO: Specify argument and return info for the @param preprocess_function.
-# TODO: nrow/dim/length
+#' @param parameters A list object whose keys are the parameters of this model. Each parameter key must be assigned a numeric vector of 3 elements: the parameter's lower bound, plausible value, and upper bound. E.g. \code{list("xi" = c(0, 0.1, 1), "ep" = c(0, 0.2, 1), "rho" = c(0, exp(2.0), Inf))}.
+#' @param regressors Names of the model-based regressors, e.g. \code{c("Qgo", "Qnogo", "Wgo", "Wnogo")}; OR if model-based regressors are not available for this model, \code{NULL}.
+#' @param preprocess_function The model-specific function to preprocess the raw data to pass to Stan. Takes a data.frame object \code{raw_data} as argument, and returns a list object \code{stan_data}.
 
 model_base <- function(task_name,
                        model_name,
@@ -83,124 +80,67 @@ model_base <- function(task_name,
       cat("These rows are removed prior to modeling the data.\n")
     }
 
-    ####################################################
-    ##   Prepare general info about the raw data   #####
-    ####################################################
-
-    subjects         <- NULL  # List of unique subjects (1D)
-    subjects_count   <- NULL  # Total number of subjects (0D)
-
-    blocks           <- NULL  # List of the different blocks per each subject (2D)
-    blocks_count     <- NULL  # Number of blocks per each subject (1D)
-    blocks_count_max <- NULL  # Maximum number of blocks across all subjects (0D)
-
-    trials_count     <- NULL  # Number of trials (per block) per subject (2D or 1D)
-    trials_count_max <- NULL  # Maximum number of trials across all blocks & subjects (0D)
-
-    # NOTE: Code is currently considering the possibility that subjects may have different
-    # naming schemes for their blocks. If willing to enforce a rule that in the data,
-    # block names must be unified across all subjects, e.g. "all block names start at 1 and
-    # count upwards", a simpler and more efficient code will be possible. For instance,
-    # `blocks_count_max <- length(unique(raw_data$block))` would be sufficient.
-
-    # TODO: Completion to here.
-    # TODO: Resume from here:
-
-#    if(model_type != "multipleB"){
-#    }
-
-#    subjects <- unique(raw_data$subjid)
-#    subjects_count <- length(subjects)
-
-#    if (model_type == "single" && subjects_count != 1) {
-#      stop("** More than 1 unique subjects exist in data file, while using 'single' type model. **\n")
-#    }
-
-#    if (model_type == "multipleB") {
-#      tmp <- NULL
-#      for (sIdx in 1:subjects_count) {
-#        tmp <- rle(sort(raw_data[raw_data$subjID == subjects[sIdx], ]$block))
-#        blocks_count_max <-
-#        blocks[tmp$values]
-
-#        blocks_count[sIdx] <- sum(rows_curr_subject$
-
-#    blocks <- array(
-#    num_blocks <- vector("integer", num_subjects)    # list of number of blocks for each subject
-#    max_num_blocks <- NA                             # maximum number of blocks for any subject
-
-#    if (model_type == "multipleB") {
-#      for (sIdx in 1:num_subjects) {
-#        curr_subject <- subjects[sIdx]
-#        rows_curr_subject <- subset(raw_data, subjID == curr_subject)
-#        block
-#        Bsubj[sIdx] <- sum(rowsCurrSubj$block == subjList[sIdx])
-#      }
-#      maxBlocks <- max(Bsubj)
-#    }
-
-#    Tsubj
-      # number of trials for each subject (or for each subject's each block if model_type == multipleB)
-#    maxTrials
-
-#    Tsubj <- as.vector(rep(0, numSubjs)) # number of trials for each subject
-
-#    for (sIdx in 1:numSubjs)  {
-#      curSubj     <- subjList[sIdx]
-#      Tsubj[sIdx] <- sum(raw_data$subjID == curSubj)  # Tsubj[N]
-#    }
-
-#    maxTrials <- max(Tsubj)
-
     #########################################################
     ##   Prepare: stan_data                             #####
     ##            stan_pars                             #####
     ##            stan_init       for passing to Stan   #####
     #########################################################
 
-    # Preprocess the raw data to make suitable for passing to Stan
+    # Preprocess the raw data to pass to Stan
     stan_data <- preprocess_function(raw_data)
 
-    # Names of the parameters of interest for Stan
-    stan_pars <- c("log_lik")
-    # TODO: paste0("mu_", parameter_names) will do the trick
-    stan_pars <- c(stan_pars, mapply(function(x) paste0("mu_", x), parameter_names))
-    stan_pars <- c(stan_pars, "sigma")
-    stan_pars <- c(stan_pars, parameter_names)
+    # The parameters of interest for Stan
+    stan_pars <- c(paste0("mu_", names(parameters)),
+                   "sigma",
+                   names(parameters),
+                   "log_lik")
 
     if (modelRegressor) {
-      stan_pars <- c(stan_pars, regressor_names)
+      stan_pars <- c(stan_pars, regressors)
     }
 
     if (inc_postpred) {
       stan_pars <- c(stan_pars, "y_pred")
     }
 
-    # inits
-    # TODO: Make another common function to define the initial values.
-    if (inits[1] != "random") {
+    # Initial values for the parameters
+    if (inits[1] == "random") {
+      stan_init <- "random"
+    } else {
       if (inits[1] == "fixed") {
-        inits_fixed <- c(1.0, 1.0, 1.0)
-      } else {
-        if (length(inits) == length(parameter_names)) {
-          inits_fixed <- inits
-        } else {
-          stop("** Check your initial values! **\n")
-        }
+        inits <- unlist(lapply(parameters, "[", 2))   # plausible values of each parameter
+      }
+      if (length(inits) != length(parameters)) {
+        stop("** Length of 'inits' must be ", length(parameters),
+             " (= the number of parameters of this model). Please check again. **\n")
       }
       stan_init <- function() {
-        list(
-          mu_p     = c( qnorm( inits_fixed[1]/2), qnorm( inits_fixed[2]/5), qnorm( inits_fixed[3]/5)),
-          sigma    = c(1.0, 1.0, 1.0),
-          rho_p    = rep(qnorm( inits_fixed[1]/2), numSubjs),
-          lambda_p = rep(qnorm( inits_fixed[2]/5), numSubjs),
-          tau_p    = rep(qnorm( inits_fixed[3]/5), numSubjs)
-        )
+        primes <- vector()
+        for (pIdx in 1:length(parameters)) {
+          lb <- parameters[[pIdx]][1]   # lower bound
+          ub <- parameters[[pIdx]][3]   # upper bound
+          if (is.finite(lb) && (lb != 0)) {
+            warning("Message to Dev: This is the first occurrence of a finite non-zero",
+                    " lower bound for a parameter. Please make sure to re-adjust the",
+                    " Stan file(s) accordingly, then move on to delete this warning.\n")
+          }
+          if (is.infinite(lb)) {
+            primes[pIdx] <- inits[pIdx]                             # (-Inf, Inf)
+          } else if (is.infinite(ub)) {
+            primes[pIdx] <- log(inits[pIdx] - lb)                   # (  lb, Inf)
+          } else {
+            primes[pIdx] <- qnorm((inits[pIdx] - lb) / (ub - lb))   # (  lb,  ub)
+          }
+        }
+        l1 <- list(mu_p  = primes,
+                   sigma = rep(1.0, length(primes)))
+        l2 <- lapply(primes, function(x) rep(x, stan_data$N))
+        names(l2) <- paste0(names(parameters), "_p")
+        return(c(l1, l2))
       }
-    } else {
-      stan_init <- "random"
     }
 
+    ############### Print info ###############
 
     # Set number of cores for parallel computing
     if (ncore <= 1) {
@@ -209,14 +149,16 @@ model_base <- function(task_name,
       local_cores <- parallel::detectCores()
       if (ncore > local_cores) {
         ncore <- local_cores
-        warning("Number of cores specified for parallel computing greater than number of locally available cores. Using all locally available cores.\n")
+        warning("Number of cores specified for parallel computing greater than",
+                " number of locally available cores. Using all locally available cores.\n")
       }
     }
     options(mc.cores = ncore)
 
     # Information for user
+    model <- paste0(task_name, "_", model_name)
     cat("\n")
-    cat("Model name  =  ", model_name, "\n", sep = "")
+    cat("Model name  =  ", model, "\n", sep = "")
     cat("Data file   =  ", data, "\n", sep = "")
     cat("\n")
     cat("Details:\n")
@@ -228,23 +170,23 @@ model_base <- function(task_name,
       cat(" # of MCMC samples (per chain)  =  ", niter, "\n", sep = "")
       cat(" # of burn-in samples           =  ", nwarmup, "\n", sep = "")
     }
-    cat(" # of subjects                  =  ", numSubjs, "\n", sep = "")
+    cat(" # of subjects                  =  ", stan_data$N, "\n", sep = "")
     if (model_type == "multipleB") {
-      cat(" # of (max) blocks per subject  =  ", maxBlocks, "\n", sep = "")
+      cat(" # of (max) blocks per subject  =  ", stan_data$maxB, "\n", sep = "")
     }
-    cat(" # of (max) trials per subject  =  ", maxTrials, "\n", sep = "")
+    cat(" # of (max) trials per subject  =  ", stan_data$T, "\n", sep = "")
+
+    ############### Fit & extract ###############
 
     # Path to .stan file
-    model_code <- paste0(model_name, ".stan")
     if (FLAG_CRAN_VERSION) {
-      modelPath <- system.file("stan", model_code, package = "hBayesDM")
-      stan_model <- rstan::stan_model(modelPath)
+      stan_model <- rstan::stan_model(system.file("stan", paste0(model, ".stan"), package = "hBayesDM"))
     } else {
-      stan_model <- stanmodels$model_name
+      stan_model <- stanmodels$model
       cat("\n")
-      cat("***********************************\n")
-      cat("**  Loading a precompiled model  **\n")
-      cat("***********************************\n")
+      cat("************************************\n")
+      cat("**  Loading a pre-compiled model  **\n")
+      cat("************************************\n")
     }
 
     # Fit the Stan model
@@ -273,62 +215,51 @@ model_base <- function(task_name,
       parVals$y_pred[parVals$y_pred == -1] <- NA
     }
 
-    # Individual parameters (e.g., individual posterior means)
-    measureIndPars <- switch(indPars, mean = mean, median = median, mode = estimate_mode) # TODO: Change to even simpler
-    allIndPars <- array(NA, c(numSubjs, length(parameter_names)))
-    allIndPars <- as.data.frame(allIndPars)
+    # Define measurement of individual parameters
+    measure_indPars <- switch(indPars, mean = mean, median = median, mode = estimate_mode)
 
-    for (i in 1:numSubjs) {
-      allIndPars[i,] <- mapply(function(param) {
-        measureIndPars(parVals[[param]][, i])
-      }, parameter_names)
+    # Measure all individual parameters (per subject)
+    allIndPars <- as.data.frame(array(NA, c(stan_data$N, length(parameters))))
+    for (sIdx in 1:stan_data$N) {
+      allIndPars[sIdx, ] <- mapply(function(x) measure_indPars(parVals[[x]][, sIdx]), names(parameters))
     }
-
-    allIndPars           <- cbind(subjList, allIndPars)
-    colnames(allIndPars) <- c("subjID", parameter_names)
-
-    # Wrap up data into a list
-    modelData                 <- list()
-    modelData$model           <- model_name
-    modelData$allIndPars      <- allIndPars
-    modelData$parVals         <- parVals
-    modelData$fit             <- fit
-    modelData$rawdata         <- raw_data
-    modelData$modelRegressor  <- NA
+    allIndPars <- cbind(stan_data$ID, allIndPars)
+    colnames(allIndPars) <- c("subjID", names(parameter))
 
     # Model regressors (for model-based neuroimaging, etc.)
     if (modelRegressor) {
       cat("\n")
-      cat("*****************************************\n")
-      cat("**  Extracting model-based regressors  **\n")
-      cat("*****************************************\n")
+      cat("**************************************\n")
+      cat("**  Extract model-based regressors  **\n")
+      cat("**************************************\n")
 
-      ev_c  <- apply(parVals$mr_ev_c, c(2, 3), measureIndPars)
-      ev_nc <- apply(parVals$mr_ev_nc, c(2, 3), measureIndPars)
-      pe    <- apply(parVals$mr_pe, c(2, 3), measureIndPars)
+      model_regressor <- lapply(regressors, function(x) apply(parVals[[x]], c(2, 3), measure_indPars))
+      names(model_regressor) <- regressors
+    }
 
-      # Initialize modelRegressor and add model-based regressors
-      regressors        <- NULL
-      regressors$ev_c   <- ev_c
-      regressors$ev_nc  <- ev_nc
-      regressors$pe     <- pe
-
-      modelData$modelRegressor <- regressors
+    # Wrap up data into a list
+    modelData                   <- list()
+    modelData$model             <- model
+    modelData$allIndPars        <- allIndPars
+    modelData$parVals           <- parVals
+    modelData$fit               <- fit
+    modelData$rawdata           <- raw_data
+    if (modelRegressor) {
+      modelData$modelRegressor  <- model_regressor
     }
 
     class(modelData) <- "hBayesDM"
 
-    # If saveDir is specified, save modelData as a file. If not, don't save.
+    # If saveDir is specified, save modelData as a file.
     if (!is.null(saveDir)) {
-      currDate  <- Sys.Date()
-      currTime  <- Sys.time()
-      currHr    <- substr(currTime, 12, 13)
-      currMin   <- substr(currTime, 15, 16)
-      timeStamp <- paste0(currDate, "_", currHr, "_", currMin)
-      # TODO: sub("\\..*$", "", basename(str)) # TODO: sub? or gsub?
-      dataFileName <- sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(data))
+      currDate     <- Sys.Date()
+      currTime     <- Sys.time()
+      currHr       <- substr(currTime, 12, 13)
+      currMin      <- substr(currTime, 15, 16)
+      timeStamp    <- paste0(currDate, "_", currHr, "_", currMin)
+      dataFileName <- sub("\\..*$", "", basename(data))
       # Save file with its model name, data info, and time stamp (date & time (hr & min))
-      save(modelData, file = file.path(saveDir, paste0(model_name, "_", dataFileName, "_", timeStamp, ".RData")))
+      save(modelData, file = file.path(saveDir, paste0(model, "_", dataFileName, "_", timeStamp, ".RData")))
     }
 
     # Inform user of completion
@@ -340,38 +271,4 @@ model_base <- function(task_name,
     return(modelData)
   }
 }
-
-# rename:
-p_f <- function(raw_data) {
-  # for multiple subjects
-  gain    <- array(0, c(numSubjs, maxTrials))
-  loss    <- array(0, c(numSubjs, maxTrials))
-  cert    <- array(0, c(numSubjs, maxTrials))
-  gamble  <- array(-1, c(numSubjs, maxTrials))
-
-  for (i in 1:numSubjs) {
-    curSubj      <- subjList[i]
-    useTrials    <- Tsubj[i]
-    tmp          <- subset(raw_data, raw_data$subjID == curSubj)
-    gain[i, 1:useTrials]    <- tmp[1:useTrials, "gain"]
-    loss[i, 1:useTrials]    <- abs(tmp[1:useTrials, "loss"])  # absolute loss amount
-    cert[i, 1:useTrials]    <- tmp[1:useTrials, "cert"]
-    gamble[i, 1:useTrials]  <- tmp[1:useTrials, "gamble"]
-  }
-
-  # TODO: re-check for all necessary data_names?
-
-  stan_data <- list(
-    N       = numSubjs,
-    T       = maxTrials,
-    Tsubj   = Tsubj,
-    gain    = gain,
-    loss    = loss,
-    cert    = cert,
-    gamble  = gamble)
-
-  return(stan_data)
-}
-
-THE_MODEL_FUNCTION <- model_base(~~~~~)
 
