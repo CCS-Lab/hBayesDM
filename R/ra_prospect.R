23a421f631c408f710302d7b87fd5fb0eb6969f5
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2018-10-12 16:07:03 +0900

    Implement the RA prospect using model_base

--- R/ra_prospect.R
+++ R/ra_prospect.R
@@ -117,245 +117,305 @@
 #' path_to_regulate_data = system.file("extdata/ra_data_reappraisal.txt", package = "hBayesDM")
 #' }
 
-ra_prospect <- function(data           = "choose",
-                        niter          = 4000,
-                        nwarmup        = 1000,
-                        nchain         = 4,
-                        ncore          = 1,
-                        nthin          = 1,
-                        inits          = "random",
-                        indPars        = "mean",
-                        saveDir        = NULL,
-                        modelRegressor = FALSE,
-                        vb             = FALSE,
-                        inc_postpred   = FALSE,
-                        adapt_delta    = 0.95,
-                        stepsize       = 1,
-                        max_treedepth  = 10) {
+ra_prospect <- model_base(
+  task_name = 'ra',
+  model_name = 'prospect',
+  data_columns = c('subjID', 'gain', 'loss', 'cert', 'gamble'),
 
-  # Path to .stan model file
-  if (modelRegressor) { # model regressors (for model-based neuroimaging, etc.)
-    stop("** Model-based regressors are not available for this model **\n")
-  }
+  parameters = list(rho = c(0, 1, 2),
+                    lambda = c(0, 1, 5),
+                    tau = c(0, 1, 5)),
 
-  # To see how long computations take
-  startTime <- Sys.time()
+  preprecess_function = function(rawdata) {
+    # list of subjects x blocks
+    subjs <- unique(rawdata$subjid)
+    # number of subjects
+    n_subj <- length(subjList)
 
-  # For using example data
-  if (data == "example") {
-    data <- system.file("extdata", "ra_exampleData.txt", package = "hBayesDM")
-  } else if (data == "choose") {
-    data <- file.choose()
-  }
+    # number of trials for each subject
+    t_subjs <- as.vector(rep(0, n_subj))
 
-  # Load data
-  if (file.exists(data)) {
-    rawdata <- read.table( data, header = T, sep = "\t")
-  } else {
-    stop("** The data file does not exist. Please check it again. **\n  e.g., data = '/MyFolder/SubFolder/dataFile.txt', ... **\n")
-  }
-  # Remove rows containing NAs
-  NA_rows_all = which(is.na(rawdata), arr.ind = T)  # rows with NAs
-  NA_rows = unique(NA_rows_all[, "row"])
-  if (length(NA_rows) > 0) {
-    rawdata = rawdata[-NA_rows,]
-    cat("The number of rows with NAs = ", length(NA_rows), ". They are removed prior to modeling the data. \n", sep = "")
-  }
-
-  # Individual Subjects
-  subjList <- unique(rawdata[,"subjID"]) # list of subjects x blocks
-  numSubjs <- length(subjList)           # number of subjects
-
-  # Specify the number of parameters and parameters of interest
-  numPars <- 3
-  POI     <- c("mu_rho", "mu_lambda", "mu_tau",
-               "sigma",
-               "rho" , "lambda", "tau",
-               "log_lik")
-
-  if (inc_postpred) {
-    POI <- c(POI, "y_pred")
-  }
-
-  modelName <- "ra_prospect"
-
-  # Information for user
-  cat("\nModel name = ", modelName, "\n")
-  cat("Data file  = ", data, "\n")
-  cat("\nDetails:\n")
-  if (vb) {
-    cat(" # Using variational inference # \n")
-  } else {
-    cat(" # of chains                   = ", nchain, "\n")
-    cat(" # of cores used               = ", ncore, "\n")
-    cat(" # of MCMC samples (per chain) = ", niter, "\n")
-    cat(" # of burn-in samples          = ", nwarmup, "\n")
-  }
-  cat(" # of subjects                 = ", numSubjs, "\n")
-
-  ################################################################################
-  # THE DATA.  ###################################################################
-  ################################################################################
-
-  Tsubj <- as.vector( rep( 0, numSubjs)) # number of trials for each subject
-
-  for (sIdx in 1:numSubjs)  {
-    curSubj     <- subjList[ sIdx]
-    Tsubj[sIdx] <- sum( rawdata$subjID == curSubj)  # Tsubj[N]
-  }
-
-  maxTrials <- max(Tsubj)
-
-  # Information for user continued
-  cat(" # of (max) trials per subject = ", maxTrials, "\n\n")
-
-  # for multiple subjects
-  gain    <- array(0, c(numSubjs, maxTrials))
-  loss    <- array(0, c(numSubjs, maxTrials))
-  cert    <- array(0, c(numSubjs, maxTrials))
-  gamble  <- array(-1, c(numSubjs, maxTrials))
-
-  for (i in 1:numSubjs) {
-    curSubj      <- subjList[i]
-    useTrials    <- Tsubj[i]
-    tmp          <- subset(rawdata, rawdata$subjID == curSubj)
-    gain[i, 1:useTrials]    <- tmp[1:useTrials, "gain"]
-    loss[i, 1:useTrials]    <- abs(tmp[1:useTrials, "loss"])  # absolute loss amount
-    cert[i, 1:useTrials]    <- tmp[1:useTrials, "cert"]
-    gamble[i, 1:useTrials]  <- tmp[1:useTrials, "gamble"]
-  }
-
-  dataList <- list(
-    N       = numSubjs,
-    T       = maxTrials,
-    Tsubj   = Tsubj,
-    numPars = numPars,
-    gain    = gain,
-    loss    = loss,
-    cert    = cert,
-    gamble  = gamble)
-
-  # inits
-  if (inits[1] != "random") {
-    if (inits[1] == "fixed") {
-      inits_fixed <- c(1.0, 1.0, 1.0)
-    } else {
-      if (length(inits) == numPars) {
-        inits_fixed <- inits
-      } else {
-        stop("Check your inital values!")
-      }
+    for (i in 1:n_subj)  {
+      subj <- subjs[i]
+      t_subjs[i] <- sum(rawdata$subjid == subj)
     }
-    genInitList <- function() {
-      list(
-        mu_p     = c( qnorm( inits_fixed[1]/2), qnorm( inits_fixed[2]/5), qnorm( inits_fixed[3]/5)),
-        sigma    = c(1.0, 1.0, 1.0),
-        rho_p    = rep(qnorm( inits_fixed[1]/2), numSubjs),
-        lambda_p = rep(qnorm( inits_fixed[2]/5), numSubjs),
-        tau_p    = rep(qnorm( inits_fixed[3]/5), numSubjs)
+
+    t_max <- max(t_subjs)
+
+    # Information for user continued
+    cat(" # of (max) trials per subject = ", t_max, "\n\n")
+
+    # for multiple subjects
+    gain    <- array(0, c(n_subj, t_max))
+    loss    <- array(0, c(n_subj, t_max))
+    cert    <- array(0, c(n_subj, t_max))
+    gamble  <- array(-1, c(n_subj, t_max))
+
+    for (i in 1:numSubjs) {
+      subj <- subjs[i]
+      t    <- t_subjs[i]
+      tmp  <- subset(rawdata, rawdata$subjid == subj)
+
+      gain[i, 1:t]   <- tmp[1:t, "gain"]
+      loss[i, 1:t]   <- abs(tmp[1:t, "loss"])  # absolute loss amount
+      cert[i, 1:t]   <- tmp[1:t, "cert"]
+      gamble[i, 1:t] <- tmp[1:t, "gamble"]
+    }
+
+    data_list <- list(
+      ID      = subjs,
+      N       = n_subj,
+      T       = t_max,
+      Tsubj   = t_subjs,
+      gain    = gain,
+      loss    = loss,
+      cert    = cert,
+      gamble  = gamble
+    )
+
+    return(data_list)
+  }
 )
-    }
-  } else {
-    genInitList <- "random"
-  }
 
-  if (ncore > 1) {
-    numCores <- parallel::detectCores()
-    if (numCores < ncore) {
-      options(mc.cores = numCores)
-      warning("Number of cores specified for parallel computing greater than number of locally available cores. Using all locally available cores.")
-    } else {
-      options(mc.cores = ncore)
-    }
-  } else {
-    options(mc.cores = 1)
-  }
-
-  cat("***********************************\n")
-  cat("**  Loading a precompiled model  **\n")
-  cat("***********************************\n")
-
-  # Fit the Stan model
-  m = stanmodels$ra_prospect
-  if (vb) {   # if variational Bayesian
-    fit = rstan::vb(m,
-                    data   = dataList,
-                    pars   = POI,
-                    init   = genInitList)
-  } else {
-    fit = rstan::sampling(m,
-                          data   = dataList,
-                          pars   = POI,
-                          warmup = nwarmup,
-                          init   = genInitList,
-                          iter   = niter,
-                          chains = nchain,
-                          thin   = nthin,
-                          control = list(adapt_delta   = adapt_delta,
-                                         max_treedepth = max_treedepth,
-                                         stepsize      = stepsize))
-  }
-  # Extract the Stan fit object
-  parVals <- rstan::extract(fit, permuted = T)
-  if (inc_postpred) {
-    parVals$y_pred[parVals$y_pred == -1] <- NA
-  }
-
-  rho    <- parVals$rho
-  lambda <- parVals$lambda
-  tau    <- parVals$tau
-
-  # Individual parameters (e.g., individual posterior means)
-  allIndPars <- array(NA, c(numSubjs, numPars))
-  allIndPars <- as.data.frame(allIndPars)
-
-  for (i in 1:numSubjs) {
-    if (indPars == "mean") {
-      allIndPars[i,] <- c( mean(rho[, i]),
-                            mean(lambda[, i]),
-                            mean(tau[, i]))
-    } else if (indPars == "median") {
-      allIndPars[i,] <- c( median(rho[, i]),
-                            median(lambda[, i]),
-                            median(tau[, i]))
-    } else if (indPars == "mode") {
-      allIndPars[i,] <- c( estimate_mode(rho[, i]),
-                            estimate_mode(lambda[, i]),
-                            estimate_mode(tau[, i]))
-    }
-  }
-
-  allIndPars           <- cbind(allIndPars, subjList)
-  colnames(allIndPars) <- c("rho",
-                            "lambda",
-                            "tau",
-                            "subjID")
-
-  # Wrap up data into a list
-  modelData        <- list(modelName, allIndPars, parVals, fit, rawdata)
-  names(modelData) <- c("model", "allIndPars", "parVals", "fit", "rawdata")
-  class(modelData) <- "hBayesDM"
-
-  # Total time of computations
-  endTime  <- Sys.time()
-  timeTook <- endTime - startTime
-
-  # If saveDir is specified, save modelData as a file. If not, don't save
-  # Save each file with its model name and time stamp (date & time (hr & min))
-  if (!is.null(saveDir)) {
-    currTime  <- Sys.time()
-    currDate  <- Sys.Date()
-    currHr    <- substr(currTime, 12, 13)
-    currMin   <- substr(currTime, 15, 16)
-    timeStamp <- paste0(currDate, "_", currHr, "_", currMin)
-    dataFileName = sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(data))
-    save(modelData, file = file.path(saveDir, paste0(modelName, "_", dataFileName, "_", timeStamp, ".RData")))
-  }
-
-  # Inform user of completion
-  cat("\n************************************\n")
-  cat("**** Model fitting is complete! ****\n")
-  cat("************************************\n")
-
-  return(modelData)
-}
+# ra_prospect <- function(data           = "choose",
+#                         niter          = 4000,
+#                         nwarmup        = 1000,
+#                         nchain         = 4,
+#                         ncore          = 1,
+#                         nthin          = 1,
+#                         inits          = "random",
+#                         indPars        = "mean",
+#                         saveDir        = NULL,
+#                         modelRegressor = FALSE,
+#                         vb             = FALSE,
+#                         inc_postpred   = FALSE,
+#                         adapt_delta    = 0.95,
+#                         stepsize       = 1,
+#                         max_treedepth  = 10) {
+#
+#   # Path to .stan model file
+#   if (modelRegressor) { # model regressors (for model-based neuroimaging, etc.)
+#     stop("** Model-based regressors are not available for this model **\n")
+#   }
+#
+#   # To see how long computations take
+#   startTime <- Sys.time()
+#
+#   # For using example data
+#   if (data == "example") {
+#     data <- system.file("extdata", "ra_exampleData.txt", package = "hBayesDM")
+#   } else if (data == "choose") {
+#     data <- file.choose()
+#   }
+#
+#   # Load data
+#   if (file.exists(data)) {
+#     rawdata <- read.table( data, header = T, sep = "\t")
+#   } else {
+#     stop("** The data file does not exist. Please check it again. **\n  e.g., data = '/MyFolder/SubFolder/dataFile.txt', ... **\n")
+#   }
+#   # Remove rows containing NAs
+#   NA_rows_all = which(is.na(rawdata), arr.ind = T)  # rows with NAs
+#   NA_rows = unique(NA_rows_all[, "row"])
+#   if (length(NA_rows) > 0) {
+#     rawdata = rawdata[-NA_rows,]
+#     cat("The number of rows with NAs = ", length(NA_rows), ". They are removed prior to modeling the data. \n", sep = "")
+#   }
+#
+#   # Individual Subjects
+#   subjList <- unique(rawdata[,"subjID"]) # list of subjects x blocks
+#   numSubjs <- length(subjList)           # number of subjects
+#
+#   # Specify the number of parameters and parameters of interest
+#   numPars <- 3
+#   POI     <- c("mu_rho", "mu_lambda", "mu_tau",
+#                "sigma",
+#                "rho" , "lambda", "tau",
+#                "log_lik")
+#
+#   if (inc_postpred) {
+#     POI <- c(POI, "y_pred")
+#   }
+#
+#   modelName <- "ra_prospect"
+#
+#   # Information for user
+#   cat("\nModel name = ", modelName, "\n")
+#   cat("Data file  = ", data, "\n")
+#   cat("\nDetails:\n")
+#   if (vb) {
+#     cat(" # Using variational inference # \n")
+#   } else {
+#     cat(" # of chains                   = ", nchain, "\n")
+#     cat(" # of cores used               = ", ncore, "\n")
+#     cat(" # of MCMC samples (per chain) = ", niter, "\n")
+#     cat(" # of burn-in samples          = ", nwarmup, "\n")
+#   }
+#   cat(" # of subjects                 = ", numSubjs, "\n")
+#
+#   ################################################################################
+#   # THE DATA.  ###################################################################
+#   ################################################################################
+#
+#   Tsubj <- as.vector( rep( 0, numSubjs)) # number of trials for each subject
+#
+#   for (sIdx in 1:numSubjs)  {
+#     curSubj     <- subjList[ sIdx]
+#     Tsubj[sIdx] <- sum( rawdata$subjID == curSubj)  # Tsubj[N]
+#   }
+#
+#   maxTrials <- max(Tsubj)
+#
+#   # Information for user continued
+#   cat(" # of (max) trials per subject = ", maxTrials, "\n\n")
+#
+#   # for multiple subjects
+#   gain    <- array(0, c(numSubjs, maxTrials))
+#   loss    <- array(0, c(numSubjs, maxTrials))
+#   cert    <- array(0, c(numSubjs, maxTrials))
+#   gamble  <- array(-1, c(numSubjs, maxTrials))
+#
+#   for (i in 1:numSubjs) {
+#     curSubj      <- subjList[i]
+#     useTrials    <- Tsubj[i]
+#     tmp          <- subset(rawdata, rawdata$subjID == curSubj)
+#     gain[i, 1:useTrials]    <- tmp[1:useTrials, "gain"]
+#     loss[i, 1:useTrials]    <- abs(tmp[1:useTrials, "loss"])  # absolute loss amount
+#     cert[i, 1:useTrials]    <- tmp[1:useTrials, "cert"]
+#     gamble[i, 1:useTrials]  <- tmp[1:useTrials, "gamble"]
+#   }
+#
+#   dataList <- list(
+#     N       = numSubjs,
+#     T       = maxTrials,
+#     Tsubj   = Tsubj,
+#     numPars = numPars,
+#     gain    = gain,
+#     loss    = loss,
+#     cert    = cert,
+#     gamble  = gamble)
+#
+#   # inits
+#   if (inits[1] != "random") {
+#     if (inits[1] == "fixed") {
+#       inits_fixed <- c(1.0, 1.0, 1.0)
+#     } else {
+#       if (length(inits) == numPars) {
+#         inits_fixed <- inits
+#       } else {
+#         stop("Check your inital values!")
+#       }
+#     }
+#     genInitList <- function() {
+#       list(
+#         mu_p     = c( qnorm( inits_fixed[1]/2), qnorm( inits_fixed[2]/5), qnorm( inits_fixed[3]/5)),
+#         sigma    = c(1.0, 1.0, 1.0),
+#         rho_p    = rep(qnorm( inits_fixed[1]/2), numSubjs),
+#         lambda_p = rep(qnorm( inits_fixed[2]/5), numSubjs),
+#         tau_p    = rep(qnorm( inits_fixed[3]/5), numSubjs)
+# )
+#     }
+#   } else {
+#     genInitList <- "random"
+#   }
+#
+#   if (ncore > 1) {
+#     numCores <- parallel::detectCores()
+#     if (numCores < ncore) {
+#       options(mc.cores = numCores)
+#       warning("Number of cores specified for parallel computing greater than number of locally available cores. Using all locally available cores.")
+#     } else {
+#       options(mc.cores = ncore)
+#     }
+#   } else {
+#     options(mc.cores = 1)
+#   }
+#
+#   cat("***********************************\n")
+#   cat("**  Loading a precompiled model  **\n")
+#   cat("***********************************\n")
+#
+#   # Fit the Stan model
+#   m = stanmodels$ra_prospect
+#   if (vb) {   # if variational Bayesian
+#     fit = rstan::vb(m,
+#                     data   = dataList,
+#                     pars   = POI,
+#                     init   = genInitList)
+#   } else {
+#     fit = rstan::sampling(m,
+#                           data   = dataList,
+#                           pars   = POI,
+#                           warmup = nwarmup,
+#                           init   = genInitList,
+#                           iter   = niter,
+#                           chains = nchain,
+#                           thin   = nthin,
+#                           control = list(adapt_delta   = adapt_delta,
+#                                          max_treedepth = max_treedepth,
+#                                          stepsize      = stepsize))
+#   }
+#   # Extract the Stan fit object
+#   parVals <- rstan::extract(fit, permuted = T)
+#   if (inc_postpred) {
+#     parVals$y_pred[parVals$y_pred == -1] <- NA
+#   }
+#
+#   rho    <- parVals$rho
+#   lambda <- parVals$lambda
+#   tau    <- parVals$tau
+#
+#   # Individual parameters (e.g., individual posterior means)
+#   allIndPars <- array(NA, c(numSubjs, numPars))
+#   allIndPars <- as.data.frame(allIndPars)
+#
+#   for (i in 1:numSubjs) {
+#     if (indPars == "mean") {
+#       allIndPars[i,] <- c( mean(rho[, i]),
+#                             mean(lambda[, i]),
+#                             mean(tau[, i]))
+#     } else if (indPars == "median") {
+#       allIndPars[i,] <- c( median(rho[, i]),
+#                             median(lambda[, i]),
+#                             median(tau[, i]))
+#     } else if (indPars == "mode") {
+#       allIndPars[i,] <- c( estimate_mode(rho[, i]),
+#                             estimate_mode(lambda[, i]),
+#                             estimate_mode(tau[, i]))
+#     }
+#   }
+#
+#   allIndPars           <- cbind(allIndPars, subjList)
+#   colnames(allIndPars) <- c("rho",
+#                             "lambda",
+#                             "tau",
+#                             "subjID")
+#
+#   # Wrap up data into a list
+#   modelData        <- list(modelName, allIndPars, parVals, fit, rawdata)
+#   names(modelData) <- c("model", "allIndPars", "parVals", "fit", "rawdata")
+#   class(modelData) <- "hBayesDM"
+#
+#   # Total time of computations
+#   endTime  <- Sys.time()
+#   timeTook <- endTime - startTime
+#
+#   # If saveDir is specified, save modelData as a file. If not, don't save
+#   # Save each file with its model name and time stamp (date & time (hr & min))
+#   if (!is.null(saveDir)) {
+#     currTime  <- Sys.time()
+#     currDate  <- Sys.Date()
+#     currHr    <- substr(currTime, 12, 13)
+#     currMin   <- substr(currTime, 15, 16)
+#     timeStamp <- paste0(currDate, "_", currHr, "_", currMin)
+#     dataFileName = sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(data))
+#     save(modelData, file = file.path(saveDir, paste0(modelName, "_", dataFileName, "_", timeStamp, ".RData")))
+#   }
+#
+#   # Inform user of completion
+#   cat("\n************************************\n")
+#   cat("**** Model fitting is complete! ****\n")
+#   cat("************************************\n")
+#
+#   return(modelData)
+# }
