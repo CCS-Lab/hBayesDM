638071e3273fa09a4acf9e4b16ebdf9f1e3b1cf3
zohyos7 <zohyos7@gmail.com>
2018-11-01 16:29:45 +0900

    Update document and precompiled files

--- src/Modules.cpp
+++ src/Modules.cpp
@@ -321,6 +321,35 @@ RCPP_MODULE(stan_fit4cra_linear_mod) {
 using namespace Rcpp ;
 #include "include/models.hpp"
 
+RCPP_MODULE(stan_fit4dbdm_prob_weight_mod) {
+
+
+    class_<rstan::stan_fit<model_dbdm_prob_weight_namespace::model_dbdm_prob_weight, boost::random::ecuyer1988> >("model_dbdm_prob_weight")
+
+    .constructor<SEXP,SEXP>()
+
+
+    .method("call_sampler", &rstan::stan_fit<model_dbdm_prob_weight_namespace::model_dbdm_prob_weight, boost::random::ecuyer1988> ::call_sampler)
+    .method("param_names", &rstan::stan_fit<model_dbdm_prob_weight_namespace::model_dbdm_prob_weight, boost::random::ecuyer1988> ::param_names)
+    .method("param_names_oi", &rstan::stan_fit<model_dbdm_prob_weight_namespace::model_dbdm_prob_weight, boost::random::ecuyer1988> ::param_names_oi)
+    .method("param_fnames_oi", &rstan::stan_fit<model_dbdm_prob_weight_namespace::model_dbdm_prob_weight, boost::random::ecuyer1988> ::param_fnames_oi)
+    .method("param_dims", &rstan::stan_fit<model_dbdm_prob_weight_namespace::model_dbdm_prob_weight, boost::random::ecuyer1988> ::param_dims)
+    .method("param_dims_oi", &rstan::stan_fit<model_dbdm_prob_weight_namespace::model_dbdm_prob_weight, boost::random::ecuyer1988> ::param_dims_oi)
+    .method("update_param_oi", &rstan::stan_fit<model_dbdm_prob_weight_namespace::model_dbdm_prob_weight, boost::random::ecuyer1988> ::update_param_oi)
+    .method("param_oi_tidx", &rstan::stan_fit<model_dbdm_prob_weight_namespace::model_dbdm_prob_weight, boost::random::ecuyer1988> ::param_oi_tidx)
+    .method("grad_log_prob", &rstan::stan_fit<model_dbdm_prob_weight_namespace::model_dbdm_prob_weight, boost::random::ecuyer1988> ::grad_log_prob)
+    .method("log_prob", &rstan::stan_fit<model_dbdm_prob_weight_namespace::model_dbdm_prob_weight, boost::random::ecuyer1988> ::log_prob)
+    .method("unconstrain_pars", &rstan::stan_fit<model_dbdm_prob_weight_namespace::model_dbdm_prob_weight, boost::random::ecuyer1988> ::unconstrain_pars)
+    .method("constrain_pars", &rstan::stan_fit<model_dbdm_prob_weight_namespace::model_dbdm_prob_weight, boost::random::ecuyer1988> ::constrain_pars)
+    .method("num_pars_unconstrained", &rstan::stan_fit<model_dbdm_prob_weight_namespace::model_dbdm_prob_weight, boost::random::ecuyer1988> ::num_pars_unconstrained)
+    .method("unconstrained_param_names", &rstan::stan_fit<model_dbdm_prob_weight_namespace::model_dbdm_prob_weight, boost::random::ecuyer1988> ::unconstrained_param_names)
+    .method("constrained_param_names", &rstan::stan_fit<model_dbdm_prob_weight_namespace::model_dbdm_prob_weight, boost::random::ecuyer1988> ::constrained_param_names)
+    ;
+}
+#include <Rcpp.h>
+using namespace Rcpp ;
+#include "include/models.hpp"
+
 RCPP_MODULE(stan_fit4dd_cs_mod) {
 
 
