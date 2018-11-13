83ec90644f71f494b4c2aa1cfe7f242650b8d88e
Jethro Lee <dlemfh96@gmail.com>
2018-11-13 21:30:44 +0900

    Rewrite bandit4arm_lapse.R using hBayesDM_model

--- R/bandit4arm_lapse.R
+++ R/bandit4arm_lapse.R
@@ -1,365 +1,71 @@
-#' 4-armed bandit task
+#' @templateVar MODEL_FUNCTION bandit4arm_lapse
+#' @templateVar TASK_NAME 4-Armed Bandit Task
+#' @templateVar MODEL_NAME 5 Parameter Model, without C (choice perseveration) but with xi (noise)
+#' @templateVar MODEL_CITE (Seymour et al., 2012, J Neuro)
+#' @templateVar MODEL_TYPE Hierarchical
+#' @templateVar DATA_COLUMNS "subjID", "choice", "gain", "loss"
+#' @templateVar PARAMETERS "Arew" (reward learning rate), "Apun" (punishment learning rate), "R" (reward sensitivity), "P" (punishment sensitivity), "xi" (noise)
+#' @templateVar LENGTH_DATA_COLUMNS 4
+#' @templateVar DETAILS_DATA_1 \item{"subjID"}{A unique identifier for each subject in the data-set.}
+#' @templateVar DETAILS_DATA_2 \item{"choice"}{Integer value representing the option chosen on the given trial: 1, 2, 3, or 4.}
+#' @templateVar DETAILS_DATA_3 \item{"gain"}{Floating point value representing the amount of currency won on the given trial (e.g. 50, 100).}
+#' @templateVar DETAILS_DATA_4 \item{"loss"}{Floating point value representing the amount of currency lost on the given trial (e.g. 0, -50).}
 #'
-#' @description
-#' Hierarchical Bayesian Modeling of the 4-armed bandit task with the following parameters: "Arew" (Reward learning rate), "Apun" (Punishment learning rate), "R" (Reward sensitivity), "P" (Punishment sensitivity), and "xi" (Noise).
-#'
-#' \strong{MODEL:}
-#' 5 parameter model without C (choice perseveration) but with xi (noise) (Seymour et al 2012, J Neuro)
-#'
-#' @param data A .txt file containing the data to be modeled. Data columns should be labelled as follows: "subjID", "choice", "gain", and "loss". See \bold{Details} below for more information.
-#' @param niter Number of iterations, including warm-up.
-#' @param nwarmup Number of iterations used for warm-up only.
-#' @param nchain Number of chains to be run.
-#' @param ncore Integer value specifying how many CPUs to run the MCMC sampling on. Defaults to 1.
-#' @param nthin Every \code{i == nthin} sample will be used to generate the posterior distribution. Defaults to 1. A higher number can be used when auto-correlation within the MCMC sampling is high.
-#' @param inits Character value specifying how the initial values should be generated. Options are "fixed" or "random" or your own initial values.
-#' @param indPars Character value specifying how to summarize individual parameters. Current options are: "mean", "median", or "mode".
-#' @param saveDir Path to directory where .RData file of model output (\code{modelData}) can be saved. Leave blank if not interested.
-#' @param modelRegressor Exporting model-based regressors? TRUE or FALSE. Currently not available for this model.
-#' @param vb             Use variational inference to approximately draw from a posterior distribution. Defaults to FALSE.
-#' @param inc_postpred Include trial-level posterior predictive simulations in model output (may greatly increase file size). Defaults to FALSE.
-#' @param adapt_delta Floating point number representing the target acceptance probability of a new sample in the MCMC chain. Must be between 0 and 1. See \bold{Details} below.
-#' @param stepsize Integer value specifying the size of each leapfrog step that the MCMC sampler can take on each new iteration. See \bold{Details} below.
-#' @param max_treedepth Integer value specifying how many leapfrog steps that the MCMC sampler can take on each new iteration. See \bold{Details} below.
-#'
-#' @return \code{modelData}  A class \code{"hBayesDM"} object with the following components:
-#' \describe{
-#'  \item{\code{model}}{Character string with the name of the model (\code{"bandit4arm_lapse"}).}
-#'  \item{\code{allIndPars}}{\code{"data.frame"} containing the summarized parameter
-#'    values (as specified by \code{"indPars"}) for each subject.}
-#'  \item{\code{parVals}}{A \code{"list"} where each element contains posterior samples
-#'    over different model parameters. }
-#'  \item{\code{fit}}{A class \code{"stanfit"} object containing the fitted model.}
-#'  \item{\code{rawdata}}{\code{"data.frame"} containing the raw data used to fit the model, as specified by the user.}
-#' }
-#'
-#' @importFrom rstan vb sampling stan_model rstan_options extract
-#' @importFrom parallel detectCores
-#' @importFrom stats median qnorm density
-#' @importFrom utils read.table
-#'
-#' @details
-#' This section describes some of the function arguments in greater detail.
-#'
-#' \strong{data} should be assigned a character value specifying the full path and name of the file, including the file extension
-#' (e.g. ".txt"), that contains the behavioral data of all subjects of interest for the current analysis.
-#' The file should be a \strong{tab-delimited} text (.txt) file whose rows represent trial-by-trial observations and columns
-#' represent variables. For the 4-armed bandit task, there should be four columns of data with the labels
-#' "subjID", "choice", "gain", and "loss". It is not necessary for the columns to be in this particular order,
-#' however it is necessary that they be labelled correctly and contain the information below:
-#' \describe{
-#'  \item{\code{"subjID"}}{A unique identifier for each subject within data-set to be analyzed.}
-#'  \item{\code{"choice"}}{A nominal integer representing which choice was chosen within the given trial (e.g. 1, 2, 3, or 4).}
-#'  \item{\code{"gain"}}{A floating number representing the amount of currency won on the given trial (e.g. 50, 50, 100).}
-#'  \item{\code{"loss"}}{A floating number representing the amount of currency lost on the given trial (e.g. 0, -50).}
-#' }
-#' \strong{*}Note: The data.txt file may contain other columns of data (e.g. "Reaction_Time", "trial_number", etc.), but only the data with the column
-#' names listed above will be used for analysis/modeling. As long as the columns above are present and labelled correctly,
-#' there is no need to remove other miscellaneous data columns.
-#'
-#' \strong{nwarmup} is a numerical value that specifies how many MCMC samples should not be stored upon the
-#' beginning of each chain. For those familiar with Bayesian methods, this value is equivalent to a burn-in sample.
-#' Due to the nature of MCMC sampling, initial values (where the sampling chain begins) can have a heavy influence
-#' on the generated posterior distributions. The \code{nwarmup} argument can be set to a high number in order to curb the
-#' effects that initial values have on the resulting posteriors.
-#'
-#' \strong{nchain} is a numerical value that specifies how many chains (i.e. independent sampling sequences) should be
-#' used to draw samples from the posterior distribution. Since the posteriors are generated from a sampling
-#' process, it is good practice to run multiple chains to ensure that a representative posterior is attained. When
-#' sampling is completed, the multiple chains may be checked for convergence with the \code{plot(myModel, type = "trace")}
-#' command. The chains should resemble a "furry caterpillar".
-#'
-#' \strong{nthin} is a numerical value that specifies the "skipping" behavior of the MCMC samples being chosen
-#' to generate the posterior distributions. By default, \code{nthin} is equal to 1, hence every sample is used to
-#' generate the posterior.
-#'
-#' \strong{Contol Parameters:} adapt_delta, stepsize, and max_treedepth are advanced options that give the user more control
-#' over Stan's MCMC sampler. The Stan creators recommend that only advanced users change the default values, as alterations
-#' can profoundly change the sampler's behavior. Refer to Hoffman & Gelman (2014, Journal of Machine Learning Research) for
-#' more information on the functioning of the sampler control parameters. One can also refer to section 58.2 of the
-#' \href{http://mc-stan.org/documentation/}{Stan User's Manual} for a less technical description of these arguments.
+#' @template model-documentation
 #'
 #' @export
+#' @include hBayesDM_model.R
 #'
 #' @references
-#'
-#' Seymour, Daw, Roiser, Dayan, & Dolan (2012) Serotonin Selectively Modulates Reward Value in Human Decision-Making. J Neuro, 32(17), 5833-5842.
-#' @seealso
-#' We refer users to our in-depth tutorial for an example of using hBayesDM: \url{https://rpubs.com/CCSL/hBayesDM}
-#'
-#' @examples
-#' \dontrun{
-#' # Run the model and store results in "output"
-#' output <- bandit4arm_lapse("example", 3000, 1000, 4, 4)  # 4 chains, 4 cores (parallel processing)
-#'
-#' # Visually check convergence of the sampling chains (should like like 'hairy caterpillars')
-#' plot(output, type = 'trace')
-#'
-#' # Check Rhat values (all Rhat values should be less than or equal to 1.1)
-#' rhat(output)
-#'
-#' # Plot the posterior distributions of the hyper-parameters (distributions should be unimodal)
-#' plot(output)
-#'
-#' # Show the WAIC and LOOIC model fit estimates
-#' printFit(output)
-#' }
-#'
-bandit4arm_lapse <- function(data           = "choose",
-                             niter          = 4000,
-                             nwarmup        = 2000,
-                             nchain         = 4,
-                             ncore          = 1,
-                             nthin          = 1,
-                             inits          = "random",
-                             indPars        = "mean",
-                             saveDir        = NULL,
-                             modelRegressor = FALSE,
-                             vb             = FALSE,
-                             inc_postpred   = FALSE,
-                             adapt_delta    = 0.95,
-                             stepsize       = 1,
-                             max_treedepth  = 10) {
+#' Seymour, Daw, Roiser, Dayan, & Dolan (2012). Serotonin Selectively Modulates Reward Value in
+#'   Human Decision-Making. J Neuro, 32(17), 5833-5842.
 
-  # Path to .stan model file
-  if (modelRegressor) { # model regressors (for model-based neuroimaging, etc.)
-    stop("** Model-based regressors are not available for this model **\n")
-  }
+bandit4arm_lapse <- hBayesDM_model(
+  task_name       = "bandit4arm",
+  model_name      = "lapse",
+  data_columns    = c("subjID", "choice", "gain", "loss"),
+  parameters      = list("Arew" = c(0, 0.1, 1),
+                         "Apun" = c(0, 0.1, 1),
+                         "R"    = c(0, 1, 30),
+                         "P"    = c(0, 1, 30),
+                         "xi"   = c(0, 0.1, 1)),
+  preprocess_func = function(raw_data, general_info) {
+    # Currently class(raw_data) == "data.table"
 
-  # To see how long computations take
-  startTime <- Sys.time()
+    # Use general_info of raw_data
+    subjs   <- general_info$subjs
+    n_subj  <- general_info$n_subj
+    t_subjs <- general_info$t_subjs
+    t_max   <- general_info$t_max
 
-  # For using example data
-  if (data == "example") {
-    data <- system.file("extdata", "bandit4arm_exampleData.txt", package = "hBayesDM")
-  } else if (data == "choose") {
-    data <- file.choose()
-  }
+    # Initialize (model-specific) data arrays
+    rew    <- array( 0, c(n_subj, t_max))
+    los    <- array( 0, c(n_subj, t_max))
+    choice <- array(-1, c(n_subj, t_max))
 
-  # Load data
-  if (file.exists(data)) {
-    rawdata <- read.table(data, header = T)
-  } else {
-    stop("** The data file does not exist. Please check it again. **\n  e.g., data = '/MyFolder/SubFolder/dataFile.txt', ... **\n")
-  }
+    # Write from raw_data to the data arrays
+    for (i in 1:n_subj) {
+      subj <- subjs[i]
+      t <- t_subjs[i]
+      DT_subj <- raw_data[subjid == subj]
 
-  # Remove rows containing NAs
-  NA_rows_all = which(is.na(rawdata), arr.ind = T)  # rows with NAs
-  NA_rows = unique(NA_rows_all[, "row"])
-  if (length(NA_rows) > 0) {
-    rawdata = rawdata[-NA_rows,]
-    cat("The number of rows with NAs = ", length(NA_rows), ". They are removed prior to modeling the data. \n", sep = "")
-  }
-
-  # Individual Subjects
-  subjList <- unique(rawdata[,"subjID"])  # list of subjects x blocks
-  numSubjs <- length(subjList)  # number of subjects
-
-  # Specify the number of parameters and parameters of interest
-  numPars <- 5
-  POI     <- c("mu_Arew", "mu_Apun", "mu_R", "mu_P", "mu_xi",
-               "sigma",
-               "Arew", "Apun", "R", "P", "xi",
-               "log_lik")
-
-  if (inc_postpred) {
-    POI <- c(POI, "y_pred")
-  }
-
-  modelName <- "bandit4arm_lapse"
-
-  # Information for user
-  cat("\nModel name = ", modelName, "\n")
-  cat("Data file  = ", data, "\n")
-  cat("\nDetails:\n")
-  if (vb) {
-    cat(" # Using variational inference # \n")
-  } else {
-    cat(" # of chains                   = ", nchain, "\n")
-    cat(" # of cores used               = ", ncore, "\n")
-    cat(" # of MCMC samples (per chain) = ", niter, "\n")
-    cat(" # of burn-in samples          = ", nwarmup, "\n")
-  }
-  cat(" # of subjects                 = ", numSubjs, "\n")
-
-  ################################################################################
-  # THE DATA.  ###################################################################
-  ################################################################################
-
-  Tsubj <- as.vector(rep(0, numSubjs)) # number of trials for each subject
-
-  for (sIdx in 1:numSubjs)  {
-    curSubj     <- subjList[sIdx]
-    Tsubj[sIdx] <- sum(rawdata$subjID == curSubj)  # Tsubj[N]
-  }
-
-  maxTrials <- max(Tsubj)
-
-  # Information for user continued
-  cat(" # of (max) trials per subject = ", maxTrials, "\n\n")
-
-  rew <- array(0, c(numSubjs, maxTrials))
-  los <- array(0, c(numSubjs, maxTrials))
-  choice <- array(-1, c(numSubjs, maxTrials))
-
-  for (subjIdx in 1:numSubjs)   {
-    #number of trials for each subj.
-    useTrials                      <- Tsubj[subjIdx]
-    currID                         <- subjList[subjIdx]
-    rawdata_curSubj                <- subset(rawdata, rawdata$subjID == currID)
-    rew[subjIdx, 1:useTrials] <- rawdata_curSubj[, "gain"]
-    los[subjIdx, 1:useTrials] <- -1 * abs(rawdata_curSubj[, "loss"])
-
-    for (tIdx in 1:useTrials) {
-      Y_t                     <- rawdata_curSubj[tIdx, "choice"] # chosen Y on trial "t"
-      choice[subjIdx , tIdx] <- Y_t
+      rew[i, 1:t]    <- DT_subj$gain
+      los[i, 1:t]    <- -1 * abs(DT_subj$loss)
+      choice[i, 1:t] <- DT_subj$choice
     }
-  }
 
-  dataList <- list(
-    N      = numSubjs,
-    T      = maxTrials,
-    Tsubj  = Tsubj ,
-    rew    = rew,
-    los    = los,
-    choice = choice
+    # Wrap into a list for Stan
+    data_list <- list(
+      N       = n_subj,
+      T       = t_max,
+      Tsubj   = t_subjs,
+      rew     = rew,
+      los     = los,
+      choice  = choice
+    )
+
+    # Returned data_list will directly be passed to Stan
+    return(data_list)
+  }
 )
-
-  # inits
-  if (inits[1] != "random") {
-    if (inits[1] == "fixed") {
-      inits_fixed = c(0.1, 0.1, 1.0, 1.0, 0.1)
-    } else {
-      if (length(inits) == numPars) {
-        inits_fixed = inits
-      } else {
-        stop("Check your inital values!")
-      }
-    }
-    genInitList <- function() {
-      list(
-        # Non-centered parameters
-        mu_p = c(qnorm(inits_fixed[1]), qnorm(inits_fixed[2]), qnorm(inits_fixed[3]/30), qnorm(inits_fixed[4]/30), qnorm(inits_fixed[5])),
-        sigma = c(1.0, 1.0, 1.0, 1.0, 1.0),
-        Arew_pr   = rep(qnorm(inits_fixed[1]), numSubjs),
-        Apun_pr   = rep(qnorm(inits_fixed[2]), numSubjs),
-        R_pr      = rep(qnorm(inits_fixed[3]/30), numSubjs),
-        P_pr      = rep(qnorm(inits_fixed[4]/30), numSubjs),
-        xi_pr      = rep(qnorm(inits_fixed[5]), numSubjs)
-)
-    }
-  } else {
-    genInitList <- "random"
-  }
-
-  # For parallel computing if using multi-cores
-  if (ncore > 1) {
-    numCores <- parallel::detectCores()
-    if (numCores < ncore) {
-      options(mc.cores = numCores)
-      warning("Number of cores specified for parallel computing greater than number of locally available cores. Using all locally available cores.")
-    } else {
-      options(mc.cores = ncore)
-    }
-  } else {
-    options(mc.cores = 1)
-  }
-
-  cat("***********************************\n")
-  cat("**  Loading a precompiled model  **\n")
-  cat("***********************************\n")
-
-  # Fit the Stan model
-  m = stanmodels$bandit4arm_lapse
-  if (vb) {   # if variational Bayesian
-    fit = rstan::vb(m,
-                    data   = dataList,
-                    pars   = POI,
-                    init   = genInitList)
-  } else {
-    fit = rstan::sampling(m,
-                          data   = dataList,
-                          pars   = POI,
-                          warmup = nwarmup,
-                          init   = genInitList,
-                          iter   = niter,
-                          chains = nchain,
-                          thin   = nthin,
-                          control = list(adapt_delta   = adapt_delta,
-                                         max_treedepth = max_treedepth,
-                                         stepsize      = stepsize))
-  }
-  ## Extract parameters
-  parVals <- rstan::extract(fit, permuted = T)
-  if (inc_postpred) {
-    parVals$y_pred[parVals$y_pred == -1] <- NA
-  }
-
-  Arew   <- parVals$Arew
-  Apun   <- parVals$Apun
-  R      <- parVals$R
-  P      <- parVals$P
-  xi     <- parVals$xi
-
-  # Individual parameters (e.g., individual posterior means)
-  allIndPars <- array(NA, c(numSubjs, numPars))
-  allIndPars <- as.data.frame(allIndPars)
-
-  for (i in 1:numSubjs) {
-    if (indPars == "mean") {
-      allIndPars[i,] <- c(mean(Arew[, i]),
-                            mean(Apun[, i]),
-                            mean(R[, i]),
-                            mean(P[, i]),
-                            mean(xi[, i]))
-    } else if (indPars == "median") {
-      allIndPars[i,] <- c(median(Arew[, i]),
-                            median(Apun[, i]),
-                            median(R[, i]),
-                            median(P[, i]),
-                            median(xi[, i]))
-    } else if (indPars == "mode") {
-      allIndPars[i,] <- c(estimate_mode(Arew[, i]),
-                            estimate_mode(Apun[, i]),
-                            estimate_mode(R[, i]),
-                            estimate_mode(P[, i]),
-                            estimate_mode(xi[, i]))
-    }
-  }
-  allIndPars           <- cbind(allIndPars, subjList)
-  colnames(allIndPars) <- c("Arew",
-                            "Apun",
-                            "R",
-                            "P",
-                            "xi",
-                            "subjID")
-
-  # Wrap up data into a list
-  modelData        <- list(modelName, allIndPars, parVals, fit, rawdata)
-  names(modelData) <- c("model", "allIndPars", "parVals", "fit", "rawdata")
-  class(modelData) <- "hBayesDM"
-
-  # Total time of computations
-  endTime  <- Sys.time()
-  timeTook <- endTime - startTime  # time took to run the code
-
-  # If saveDir is specified, save modelData as a file. If not, don't save
-  # Save each file with its model name and time stamp (date & time (hr & min))
-  if (!is.null(saveDir)) {
-    currTime  <- Sys.time()
-    currDate  <- Sys.Date()
-    currHr    <- substr(currTime, 12, 13)
-    currMin   <- substr(currTime, 15, 16)
-    timeStamp <- paste0(currDate, "_", currHr, "_", currMin)
-    dataFileName = sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(data))
-    save(modelData, file = file.path(saveDir, paste0(modelName, "_", dataFileName, "_", timeStamp, ".RData")))
-  }
-
-  # Inform user of completion
-  cat("\n************************************\n")
-  cat("**** Model fitting is complete! ****\n")
-  cat("************************************\n")
-
-  return(modelData)
-}
 
