5ad6c6363d8ff0a6a9669dbb3338145db0fc421b
Jethro Lee <dlemfh96@gmail.com>
2018-12-04 14:44:20 +0900

    Unify parameter beta upper bound to 10 in prl task models

--- R/prl_fictitious_rp.R
+++ R/prl_fictitious_rp.R
@@ -32,7 +32,7 @@ prl_fictitious_rp <- hBayesDM_model(
   parameters      = list("eta_pos" = c(0, 0.5, 1),
                          "eta_neg" = c(0, 0.5, 1),
                          "alpha"   = c(-Inf, 0, Inf),
-                         "beta"    = c(0, 1, 5)),
+                         "beta"    = c(0, 1, 10)),
   regressors      = list("ev_c"  = 2,
                          "ev_nc" = 2,
                          "pe_c"  = 2,
