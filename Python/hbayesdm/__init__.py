177044350dc2e3c318940bd8b5b1c50dab867634
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2019-08-22 03:36:22 +0900

    Do not use a separate file for versioning

--- Python/hbayesdm/__init__.py
+++ Python/hbayesdm/__init__.py
@@ -1,4 +1,3 @@
-from hbayesdm.version import version as __version__
 from hbayesdm.diagnostics import rhat, print_fit, hdi, plot_hdi
 
-__all__ = ['__version__', 'rhat', 'print_fit', 'hdi', 'plot_hdi']
+__all__ = ['rhat', 'print_fit', 'hdi', 'plot_hdi']
