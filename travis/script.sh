c8ea9209c248349e646d4abb355c2b492b19c7e0
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2019-08-31 05:59:54 +0900

    Update travis codes

--- travis/script.sh
+++ travis/script.sh
@@ -2,12 +2,12 @@
 
 # Scripts for R
 if [ "$TARGET" = "R" ]; then
-  travis_wait 42 R CMD build . --no-build-vignettes --no-manual
-  travis_wait 59 R CMD check hBayesDM*.tar.gz --as-cran --no-build-vignettes --no-manual
+  R CMD build .
+  R CMD check hBayesDM*.tar.gz
 
 # Scripts for Python
 elif [ "$TARGET" = "Python" ]; then
-  travis_wait 30 pytest tests/test_ra_prospect.py --reruns 5
+  pytest tests/test_ra_prospect.py --reruns 5
 
 # Otherwise
 else
