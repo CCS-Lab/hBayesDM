17a41ad5a1185104996a019eae01d37a855d57b5
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2019-08-31 05:19:58 +0900

    No testthat

--- travis/setup.sh
+++ travis/setup.sh
@@ -2,20 +2,6 @@
 
 # Setup codes for R
 if [ "$TARGET" = "R" ]; then
-  # export R_LIBS_USER=~/R/Library
-  # export R_LIBS_SITE=/usr/local/lib/R/site-library:/usr/lib/R/site-library
-  # export _R_CHECK_CRAN_INCOMING_=false
-  # export R_PROFILE=~/.Rprofile.site
-
-  # # Add CRAN as an APT source
-  # sudo echo 'deb https://cloud.r-project.org/bin/linux/ubuntu trusty-cran35/' >> /etc/apt/sources.list
-  # sudo apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
-  # sudo apt-get update
-
-  # # Install R with the latest version
-  # sudo apt-get install -y -q --allow-unauthenticated r-base r-base-core r-base-dev
-  # hash -r
-
   # Setup a config for R
   mkdir -p ~/.R/
   echo "CC = ${CC}" >> ~/.R/Makevars
@@ -25,7 +11,7 @@ if [ "$TARGET" = "R" ]; then
 
   # Install R packages
   Rscript \
-    -e 'install.packages(c("devtools", "roxygen2", "testthat", "covr"), quiet = T, repos = "https://cran.rstudio.com")' \
+    -e 'install.packages(c("devtools", "roxygen2", "covr"), quiet = T, repos = "https://cran.rstudio.com")' \
     -e 'devtools::install_deps(dep = T, quiet = T)'
 
 # Setup codes for Python
