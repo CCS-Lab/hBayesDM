18d0d9f42f224547d715cad3226b5cdbcf738100
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2019-08-31 01:42:27 +0900

    Define PKG_TARBALL before deploying

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -5,6 +5,7 @@ if [ "$TARGET" = "R" ]; then
   cd $ROOTPATH/R
   Rscript -e 'remotes::install_cran("pkgdown", quiet = T, repos = "https://cran.rstudio.com")'
   R CMD build .
+  export PKG_TARBALL=$(Rscript -e 'pkg <- devtools::as.package("."); cat(paste0(pkg$package,"_",pkg$version,".tar.gz"))')
   Rscript -e 'pkgdown::deploy_site_github()'
 
 # Otherwise
