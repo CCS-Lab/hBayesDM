7434c17a93190c6b382b15597a69c5d3ecf58053
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2020-12-15 17:53:22 +0900

    Update travis settings

--- travis/deploy-develop.sh
+++ travis/deploy-develop.sh
@@ -3,8 +3,9 @@
 # Scripts for R
 if [ "$TARGET" = "R" ]; then
   echo "$(pwd)"
+  # export PKG_TARBALL=$(Rscript -e 'pkg <- devtools::as.package("."); cat(paste0(pkg$package,"_",pkg$version,".tar.gz"))')
   Rscript -e 'install.packages("pkgdown")'
-  Rscript -e "pkgdown::deploy_site_github('$ROOTPATH/R')"
+  Rscript -e "pkgdown::deploy_site_github()"
 
 # Otherwise
 else
