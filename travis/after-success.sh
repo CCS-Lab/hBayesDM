64203d6ad013961440ebee7f86dfc26ac2622d76
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2020-12-15 23:23:36 +0900

    Integrate deploy code into after-success

--- travis/after-success.sh
+++ travis/after-success.sh
@@ -5,6 +5,11 @@ if [ "$TARGET" = "R" ]; then
   travis_wait 40 Rscript -e 'covr::codecov()'
   cat hBayesDM.Rcheck/00*
 
+  if [ "$TRAVIS_BRANCH" = "develop" ]; then
+    Rscript -e 'install.packages("pkgdown")'
+    Rscript -e "pkgdown::deploy_site_github()"
+  fi
+
 # Scripts for Python
 elif [ "$TARGET" = "Python" ]; then
   flake8 hbayesdm --format=pylint --statistics --exit-zero
