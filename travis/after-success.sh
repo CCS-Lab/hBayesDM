b1ead023ff4fcf9cfd9b1442f2db25c53e01255d
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2020-12-16 12:13:55 +0900

    Remove deploy-related codes

--- travis/after-success.sh
+++ travis/after-success.sh
@@ -5,13 +5,6 @@ if [ "$TARGET" = "R" ]; then
   travis_wait 40 Rscript -e 'covr::codecov()'
   cat hBayesDM.Rcheck/00*
 
-  if [ "$TRAVIS_BRANCH" = "develop" ]; then
-    R CMD build .
-    export PKG_TARBALL="$(ls *.tar.gz)"
-    Rscript -e 'install.packages("pkgdown")'
-    Rscript -e "pkgdown::deploy_site_github()"
-  fi
-
 # Scripts for Python
 elif [ "$TARGET" = "Python" ]; then
   flake8 hbayesdm --format=pylint --statistics --exit-zero
